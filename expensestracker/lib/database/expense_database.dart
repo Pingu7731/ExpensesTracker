2c30f86ca8691d040da106f435c53afca1779559
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-08-26 03:45:58 +0800

    init isar db
    -added isar db and did the init
    -added provider
    -running db + my app

---
+++ expensestracker/lib/database/expense_database.dart
@@ -0,0 +1,49 @@
+import 'package:expensestracker/model/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ExpenseDb extends ChangeNotifier {
+  static late Isar isar;
+  List<Expense> _allexpense = [];
+
+  //init db
+
+  static Future<void> initialize() async {
+    final dir = await getApplicationCacheDirectory();
+    isar = await Isar.open([ExpenseSchema], directory: dir.path);
+  }
+
+  //get all expenses
+  List<Expense> get allExpense => _allexpense;
+
+  //create
+  Future<void> createExpenses(Expense newExpense) async {
+    await isar.writeTxn(() => isar.expenses.put(newExpense));
+    //read the list again
+    readExpenses();
+  }
+
+  //read
+  Future<void> readExpenses() async {
+    List<Expense> fetchedExpenses = await isar.expenses.where().findAll();
+
+    _allexpense.clear();
+    _allexpense.addAll(fetchedExpenses);
+
+    //update the ui
+    notifyListeners();
+  }
+
+  // update
+  Future<void> updateExpenses(int id, Expense updatedExpense) async {
+    updatedExpense.id = id;
+    await isar.writeTxn(() => isar.expenses.put(updatedExpense));
+    await readExpenses();
+  }
+
+  // delete
+  Future<void> deleteExpenses(int id) async {
+    await isar.writeTxn(() => isar.expenses.delete(id));
+  }
+}
--- expensestracker/lib/main.dart
+++ expensestracker/lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:expensestracker/database/expense_database.dart';
 import 'package:expensestracker/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+
+  await ExpenseDb.initialize();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ExpenseDb(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
