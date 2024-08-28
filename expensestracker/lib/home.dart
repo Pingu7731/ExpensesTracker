6445cfd58e0686b958306825743696c76ceefc57
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-08-29 03:44:01 +0800

    added add button
    -at the bottom right

--- expensestracker/lib/home.dart
+++ expensestracker/lib/home.dart
@@ -8,8 +8,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController amountcontroller = TextEditingController();
+
+  void openNewExpenseBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text(
+          'Da Fuq You Buy Again?',
+          textAlign: TextAlign.center,
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: namecontroller,
+              decoration: const InputDecoration(hintText: "Whachu buy"),
+            ),
+            TextField(
+              controller: amountcontroller,
+              decoration: const InputDecoration(hintText: "\$ ?"),
+            ),
+          ],
+        ),
+        actions: [],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNewExpenseBox,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
   }
+}
+
+Widget _cancelbutton() {
+  return MaterialButton(
+    onPressed: () {},
+  );
 }
